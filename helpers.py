a0c17177a56fbb9deb77ad2193a030545174372d
Henry <artimax32@gmail.com>
2020-09-09 09:03:30 +0300

    Valmistatud katsetamiseks.

--- helpers.py
+++ helpers.py
@@ -3,8 +3,24 @@ elemendid = []
 # lisame ELEMENT juurde
 
 def lisa_element(nimetus, hind, kogus):
+    global elemendid
     elemendid.append({'nimetus':nimetus, 'hind':hind, 'kogus':kogus})
 
 # lisame ELEMENDID KORRAGE juurde
 def lisa_elemendid(elemendid_nimekiri):
+    global elemendid
     elemendid = elemendid_nimekiri
+
+# loome katse andmestik
+katse_elemendid = [
+        {'nimetus': 'leib', 'hind': 0.80, 'kogus': 20},
+        {'nimetus': 'piim', 'hind': 0.50, 'kogus': 15},
+        {'nimetus': 'vein', 'hind': 5.60, 'kogus': 5},
+]
+
+# testime elementide lisamist.
+lisa_elemendid(katse_elemendid)
+print(elemendid)
+
+lisa_element('kohupiim', 0.80, 15)
+print(elemendid)
